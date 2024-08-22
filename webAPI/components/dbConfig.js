bf18afb7bd3a8bb46df415acb79a5d1ec2657520
SvenskaBarnet <63249655+SvenskaBarnet@users.noreply.github.com>
2024-08-22 13:06:48 +0200

    Removed dbConfig.js from tracking

--- webAPI/components/dbConfig.js
+++
@@ -1,7 +0,0 @@
-// Used to store the connection details for the database
-module.exports = {
-    host: "localhost",
-    user: "root",
-    password: "Jagskaslappa2024!",
-    database: "newsextractdb"
-};
